c27cb542ae5bf37e3952988f0cfe1c62759bbc72
RafaelCardoso990 <rafaelcardoso990@gmail.com>
2022-05-19 23:36:09 -0300

    fix: ultimo commit

--- index.js
+++ index.js
@@ -23,6 +23,14 @@ app.use(cors({
     credentials: true
 }));
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
+
 app.use(checkoutRouter);
 app.use(userRouter);
 app.use(cartRouter);
