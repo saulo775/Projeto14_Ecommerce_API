1dccd55c967c37f940201f530144daba45d72449
saulo775 <59702227+saulo775@users.noreply.github.com>
2022-05-20 16:55:04 -0300

    Dando acesso restrito ao meu site

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ dotenv.config();
 app.use(cors());
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Origin", 'https://saia-de-filo-ecommerce.vercel.app/');
     res.header("Access-Control-Allow-Credentials", true);
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
